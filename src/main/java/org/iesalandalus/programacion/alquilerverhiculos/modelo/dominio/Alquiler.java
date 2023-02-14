0bc5d4fdc71fc70ea9a14b1ca115b4e70933bd35
ppcf_ <ppcf_@DESKTOP-H39U3KR>
2023-02-14 14:21:00 +0100

    Cuarto Commit: Creación Clase Alquiler
    Declaración de expresiones regulares, constantes, métodos,
    constructores, setter&getters. Hash, Equals y toString()

---
+++ src/main/java/org/iesalandalus/programacion/alquilerverhiculos/modelo/dominio/Alquiler.java
@@ -0,0 +1,163 @@
+package org.iesalandalus.programacion.alquilerverhiculos.modelo.dominio;
+
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.time.temporal.ChronoUnit;
+import java.util.Objects;
+
+public class Alquiler {
+	
+
+	
+	//Expresiones regulares y constantes
+	
+	public final static DateTimeFormatter FORMATO_FECHA = DateTimeFormatter.ofPattern("dd/MM/yyyy");
+	private final static int PRECIO_DIA = 20;
+	
+	//Declaración de atributos
+	
+	private Cliente cliente;
+	private Turismo turismo;
+	
+	private LocalDate fechaAlquiler;
+	private LocalDate fechaDevolucion;
+	
+	//Constructor con parámetros
+	public Alquiler(Cliente cliente, Turismo turismo, LocalDate fechaAlquiler)
+	{
+		setCliente(cliente);
+		setTurismo(turismo);
+		setFechaAlquiler(fechaAlquiler);
+	}
+	
+	//Constructor copia
+	public Alquiler(Alquiler alquiler)
+	{
+		if (alquiler == null)
+		{
+			throw new NullPointerException("ERROR: No es posible copiar un alquiler nulo.");
+		}
+		setCliente(alquiler.getCliente());
+		setTurismo(alquiler.getTurismo());
+		setFechaAlquiler(alquiler.getFechaAlquiler());
+	}
+	
+	// getters
+	public Cliente getCliente() {
+		return cliente;
+	}
+	public Turismo getTurismo() {
+		return turismo;
+	}
+	public LocalDate getFechaAlquiler() {
+		return fechaAlquiler;
+	}
+	public LocalDate getFechaDevolucion() {
+		return fechaDevolucion;
+	}
+	
+	//Setters
+	
+	
+	private void setCliente(Cliente cliente) 
+	{
+		if (cliente == null)
+		{
+			throw new NullPointerException("ERROR: El cliente no puede ser nulo.");
+		}
+		this.cliente = cliente;
+	}
+	
+	private void setTurismo(Turismo turismo) 
+	{
+		if (turismo == null)
+		{
+			throw new NullPointerException("ERROR: El turismo no puede ser nulo.");
+		}
+		this.turismo = turismo;
+	}
+	
+	//método set para evaluar que la fecha de alquiler no sea nula ni posterior a la fecha actual. 
+	private void setFechaAlquiler(LocalDate fechaAlquiler)
+	{
+		if (fechaAlquiler == null)
+		{
+			throw new NullPointerException("ERROR: La fecha de alquiler no puede ser nula.");
+		}
+		if (fechaAlquiler.isAfter(LocalDate.now()))
+		{
+			throw new IllegalArgumentException("ERROR: La fecha de alquiler no puede ser futura.");
+		}
+		this.fechaAlquiler = fechaAlquiler;
+	}
+	
+	//Este método comprueba que la devolución no sea nula, ni posterior a día de hoy, ni tampoco anterior o igual a la fecha de alquiler
+	private void setFechaDevolucion(LocalDate fechaDevolucion)
+	{
+		if (fechaDevolucion == null)
+		{
+			throw new NullPointerException("ERROR: La fecha de devolución no puede ser nula.");
+		}
+		if (fechaDevolucion.isAfter(LocalDate.now()))
+		{
+			throw new IllegalArgumentException("ERROR: La fecha de devolución no puede ser futura.");
+		}
+		if (fechaDevolucion.isAfter(fechaAlquiler) || fechaDevolucion.isEqual(fechaAlquiler))
+		{
+			throw new IllegalArgumentException("ERROR: La fecha de devolución debe ser posterior a la fecha de alquiler.");
+		}
+		this.fechaDevolucion = fechaDevolucion;
+	}
+	
+	//Método Devolver
+	
+	public void devolver(LocalDate fechaDevolucion)
+	{
+		setFechaDevolucion(fechaDevolucion);
+	}
+	
+	public int getPrecio()
+	{
+		//declaramos la variable local de tipo entero precio
+		int precio;
+		//Extraemos el factor cilindrada del atributo cilindrada de turismo y lo dividimos por 10
+		int factorCilindrada = (int) (turismo.getCilindrada()/10);
+		//Extraemos la cantidad de días entre fechas de alquiler y devolución y lo casteamos a un entero
+		int numDias = (int) ChronoUnit.DAYS.between(fechaDevolucion, fechaAlquiler); 
+		
+		//establecemos el valor de precio con la fórmula establecida en la tarea.
+		precio = (PRECIO_DIA + factorCilindrada)*numDias;
+		
+		//retornamos el valor de precio
+		return precio;
+	}
+
+	// hashCode y equals que verifican que dos objetos son iguales si su cliente, turismo y fechaAlquier son los mismos
+	@Override
+	public int hashCode() {
+		return Objects.hash(cliente, fechaAlquiler, turismo);
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Alquiler other = (Alquiler) obj;
+		return Objects.equals(cliente, other.cliente) && Objects.equals(fechaAlquiler, other.fechaAlquiler)
+				&& Objects.equals(turismo, other.turismo);
+	}
+
+	@Override
+	public String toString() {
+		return String.format("%s <---> %s, %s - %s (%d€)", cliente, turismo, fechaAlquiler, fechaDevolucion);
+	}
+	
+	
+	
+	
+
+}
