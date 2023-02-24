c7327f2873faffa05ad968d7fe0a59d51c47d508
ppcf_ <ppcf_@DESKTOP-H39U3KR>
2023-02-24 14:07:55 +0100

    Edición de MainApp

--- src/main/java/org/iesalandalus/programacion/alquilervehiculos/MainApp.java
+++ src/main/java/org/iesalandalus/programacion/alquilervehiculos/MainApp.java
@@ -4,10 +4,21 @@ import org.iesalandalus.programacion.alquilervehiculos.controlador.Controlador;
 import org.iesalandalus.programacion.alquilervehiculos.modelo.Modelo;
 import org.iesalandalus.programacion.alquilervehiculos.vista.Vista;
 
+
+/**
+ * 
+ * @author Pedro Patricio Cárdenas Figueroa
+ * Github: https://github.com/ptrdck/AlquilerVehiculos.git
+ * Tarea Online 7
+ * Programación DAM 2022/23
+ */
 public class MainApp {
 
 	public static void main(String[] args) {
-		// Ánimo!!!!
+		Modelo modelo = new Modelo();
+		Vista vista = new Vista();
+		Controlador controlador = new Controlador(modelo, vista);
+		controlador.comenzar();
 	}
 
 }
